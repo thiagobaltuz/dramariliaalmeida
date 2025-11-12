1f2fc99c7116c2016bf8f2a1e6db11f1a251d7f3
thiagobaltuz <thiagobaltuz@gmail.com>
2025-11-11 23:07:05 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Configuração para App Router no Vercel
+  experimental: {
+    appDir: true,
+  },
+  // Para garantir que funciona no Vercel
   trailingSlash: true,
   images: {
     unoptimized: true
